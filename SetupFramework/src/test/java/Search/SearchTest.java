7033f1650906afb762f6e3423d341c6ab0ae2e49
Jency Saral J <36329640+jencysaralj@users.noreply.github.com>
2021-05-15 22:04:08 +0530

    Update SearchTest.java

--- SetupFramework/src/test/java/Search/SearchTest.java
+++ SetupFramework/src/test/java/Search/SearchTest.java
@@ -60,4 +60,27 @@ public class SearchTest {
         Assert.assertEquals(showResults,"No products found");
         //driver.close();
     }
+    @Test
+    public static void MessageIfItemGotAddedToCart() {
+        System.setProperty("webdriver.chrome.driver", "/Users/jency.saral/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://spree-vapasi.herokuapp.com/");
+        WebElement clickSearchBar = driver.findElement(By.id("keywords"));
+        clickSearchBar.click();
+        String toSearchProduct = "shirt";
+        WebElement textToSearch = driver.findElement(By.id("keywords"));
+        textToSearch.sendKeys(toSearchProduct);
+        WebElement search_button = driver.findElement(By.cssSelector("#search-bar > form > input.btn.btn-success"));
+        search_button.click();
+        //SearchTest.searchProduct();
+        //driver.get(SearchTest.searchProduct());
+        String showResults = driver.findElement(By.className("search-results-title")).getText();
+        Assert.assertNotEquals(showResults, "No products found");
+        WebElement clickOnFirstProduct = driver.findElement(By.xpath("//*[@id=\"product_5\"]/div/div[1]/a/img"));
+        clickOnFirstProduct.click();
+        WebElement clickAddToCart = driver.findElement(By.id("add-to-cart-button"));
+        clickAddToCart.click();
+       // String cartTitle = driver.findElement(By.cssSelector("#content > div > h1")).getText();
+        //Assert.assertEquals(cartTitle,"Shopping Cart");
+    }
 }
